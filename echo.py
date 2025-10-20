9c7bd8b088b4dbfd5936c295cf937b6d8a509183
zwxwinston <250301384@qq.com>
2025-10-20 18:33:01 +0800

    refactor: alternative prompt on conflict branch

--- echo.py
+++ echo.py
@@ -1,13 +1,12 @@
 def echo():
-    message = input("Enter something: ")
+    message = input("Enter something:")
     print(message)
 if __name__ == "__main__":
     echo()
 
-import sys
+    import sys
 def echo():
     shout = "-s" in sys.argv
-    message = input("Enter something: ")
+    message = input("Enter something:")
     print(message.upper() if shout else message)
 if __name__ == "__main__":
-    echo()
